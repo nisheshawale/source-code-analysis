6442233fa3577b317f9889cfe53a48abed1ad2d1
nisheshfuse <nishesh@fusemachines.com>
2023-08-10 16:54:17 +0545

    [feat]: Write outputs to a file

--- name_parameters_return_value.py
+++ name_parameters_return_value.py
@@ -3,17 +3,22 @@ import argparse
 
 
 def find_name_arguments_return_type(translation_unit):
-    for node in translation_unit.cursor.walk_preorder():
-        if node.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-            print(f"Function Name :- {node.spelling}")
-            try:
-                args = node.type.argument_types()
-                for arg in args:
-                    print(f"Function Arguments :- {arg.spelling}")
-            except:
-                print(f"Function Arguments :- None")
+    with open("files/outputs/name_parameters_return_value.txt", "w") as f:
+        for node in translation_unit.cursor.walk_preorder():
+            if node.kind == clang.cindex.CursorKind.FUNCTION_DECL:
+                # print(f"Function Name :- {node.spelling}")
+                f.write(f"Function Name :- {node.spelling} \n")
+                try:
+                    args = node.type.argument_types()
+                    for arg in args:
+                        # print(f"Function Arguments :- {arg.spelling}")
+                        f.write(f"Function Arguments :- {arg.spelling} \n")
+                except:
+                    # print(f"Function Arguments :- None")
+                    f.write(f"Function Arguments :- None \n")
 
-            print(f"Return type :- {node.result_type.spelling}")
+                # print(f"Return type :- {node.result_type.spelling}")
+                f.write(f"Return type :- {node.result_type.spelling} \n")
 
 
 if __name__ == "__main__":
@@ -26,7 +31,7 @@ if __name__ == "__main__":
     c_file_path = args.file
     library_path = args.library_path
 
-    # clang.cindex.Config.set_library_path("/home/nishesh/Desktop/source_code_study/source-code-env/lib64/python3.10/site-packages/clang")
+    # clang.cindex.Config.set_library_path("/home/nishesh/Desktop/source-code-analysis/source-code-env/lib64/python3.10/site-packages/clang")
     clang.cindex.Config.set_library_path(library_path)
 
     index = clang.cindex.Index.create()
